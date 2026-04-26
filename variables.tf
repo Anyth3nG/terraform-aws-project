9000b2524f8fed0e96068012cddf924362408656
Anyth3nG <daniel2233x@gmail.com>
2026-04-26 17:34:08 +0300

    add provider and variables

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+    description = "AWS region to deploy resources"
+    type = string
+    default = "eu-west-1"
+}
+
+variable "project_name" {
+    description = "Name prefix for all resources"
+    type = string
+    default = "tf-project"
+  
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for the VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+    description = "CIDR block for the public subnet"
+    type = string
+    default = "10.0.1.0/24" 
+  
+}
+
+variable "instance_type" {
+    description = "EC2 instance type"
+    type = string
+    default = "t3.micro"
+  
+}
+
+
