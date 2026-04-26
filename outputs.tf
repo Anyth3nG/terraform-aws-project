6e282434ee6a6512743c2c8c7e491e082b41fecf
Anyth3nG <daniel2233x@gmail.com>
2026-04-26 19:02:49 +0300

    add outputs for EC2 IP, instance ID, bucket name and VPC ID and move the bucket from the outputs to main

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,22 @@
 
-# S3 Bucket
-resource "aws_s3_bucket" "main" {
-    bucket = "${var.project_name}-bucket-${random_id.suffix.hex}"
-
-    tags = {
-        Name = "${var.project_name}-bucket"
-        Project = var.project_name
-    }
+output "ec2_public_ip" {
+    description = "Public IP of the EC2 instance"
+    value = aws_instance.main.public_ip
 }
 
-# Random ID for unique bucket name
-resource "random_id" "suffix" {
-    byte_length = 4
+output "ec2_instance_id" {
+    description = "ID of the EC2 instance"
+    value = aws_instance.main.id
 }
 
-# Block all public access
-resource "aws_s3_bucket_public_access_block" "main" {
-    bucket = aws_s3_bucket.main.id
-
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+output "s3_bucket_name" {
+    description = "Name of the S3 bucket"
+    value = aws_s3_bucket.main.bucket
 }
 
-# Enable versioning 
-resource "aws_s3_bucket_versioning" "main" {
-    bucket = aws_s3_bucket.main.id
-
-    versioning_configuration {
-        status = "Enabled"
-    }
+output "vpc_id" {
+    description = "ID of the VPC"
+    value = aws_vpc.main.id
 }
+
 
